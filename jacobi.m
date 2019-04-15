ded99eefa77f9481b91bca2cfdfe9d9640701f58
ivano696 <49634797+ivano696@users.noreply.github.com>
2019-04-15 12:36:22 +0200

    Add files via upload

--- jacobi.m
+++ jacobi.m
@@ -5,17 +5,23 @@ function [x,niter,resrel] = jacobi(A,b,TOL,MAXITER)
  %Controlli numero input
    if(nargin<2)
         error("Inserire come input almeno A e b.")
+   elseif(nargin==2)
+       TOL=10^-6;
+       MAXITER=500;
+   elseif(nargin==3)
+       MAXITER=500;
    end
-
   
  %Controlli sulla matrice
- if(nargin==2)
+
     if(~ismatrix(A))
        error("Il primo input deve essere una matrice.")
     elseif(~issparse(A)||isempty(A))
        error("La matrice deve essere sparsa.")
     elseif(size(A,1)~=size(A,2))
         error("La matrice deve essere quadrata.")
+    elseif(length(A)<100)
+        error("La matrice deve essere di grandi dimensioni(almeno 100x100)")
     elseif(~isnumeric(A) ||~isreal(A)||any(any(~isfinite(A)))|| any(any(~isa(A,'double')))||any(any(isnan(A))))
       error("Gli elementi della matrice devono essere numeri reali double finiti.")
     end
@@ -28,41 +34,39 @@ function [x,niter,resrel] = jacobi(A,b,TOL,MAXITER)
    elseif(any(ischar(b))||any(~isfinite(b)) ||  any(~isreal(b))||any(~isa(b,'double'))||any(isnan(b)))
        error('Errore, b deve contenere reali finiti double.')
    end
-    
-     TOL=10^-6;
-     MAXITER=500;
- end
-  
 
-  if(nargin==3) 
+  %Controllo che la diagonale non abbia elementi nulli
+   if any(find(abs(diag(A))<=eps(norm(A))))==1
+       error("Sono presenti elementi nulli all'interno della diagonale principale.")
+   end
+   %Controllo sulla convergenza
+  if(abs(diag(A))<=abs(sum(A,2)-diag(A)))
+       error("La matrice non ha diagonale strettamante dominante. Il metodo non converge.")
+  end
+ 
   %Verifica se TOL è uno scalare, e se lo è verifica se è ammissibile
    if(~isscalar(TOL))
        error('Errore, TOL deve essere uno scalare')
-   elseif (~isfinite(TOL) ||  ~isreal(TOL) || ischar(TOL)||any(isnan(TOL)))
+   elseif (~isfinite(TOL) ||  ~isreal(TOL) || ischar(TOL)||isnan(TOL))
         error('Errore, TOL deve essere settato come un numero reale.')
    end
-   MAXITER=500;
-  end
+
    
-if(nargin==4)
+
   %Verifica se MAXITER è uno scalare, e se lo è verifica se è ammissibile;
    if(~isscalar(MAXITER))
        error('Errore, MAXITER deve essere uno scalare')
    elseif (~isfinite(MAXITER) ||  ~isreal(MAXITER) || ischar(MAXITER)||isnan(MAXITER))
         error('Errore, MAXITER deve essere settato come un numero reale finito.')
    end
-end
-  
-   %Controlli ipotesi Jacobi
-   
-   
-      
+
+     
    %Verifica se TOL appartiene a dei valori corretti
-   if(sign(TOL)<=0||TOL<eps)
+   if(sign(TOL)<=0||TOL<10^-6)
       TOL=10^-6;
       warning('Valore TOL errato. Utilizzo valore di default.') 
    end
-    %Verifica se NMAX appartiene a dei valori corretti
+    %Verifica se MAXITER appartiene a dei valori corretti
    if(MAXITER<2||sign(MAXITER)<=0|| MAXITER>500)
       MAXITER=500;
       warning('Valore MAXITER errato. Utilizzo valore di default.') 
